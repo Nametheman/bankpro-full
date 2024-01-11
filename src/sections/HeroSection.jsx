77f4483b3df2e9690e923d287ef31b626c69481d
Emmanuel Owolabi <owoemma59@gmail.com>
2024-01-11 10:30:03 +0100

    fixed gsap scroll

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -49,10 +49,15 @@ const HeroSection = () => {
           ease: "sine.out",
           scrollTrigger: {
             trigger: mockupTriggerPoint,
-            start: "top top",
-            end: "+=500",
-            scrub: 3,
-            snap: 1 / (mockupArray.length - 1),
+            start: "300 top",
+            end: "+=400",
+            scrub: 1,
+            snap: {
+              snapTo: "mockup", // snap to the closest label in the timeline
+              duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+              delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+              ease: "power1.inOut", // the ease of the snap animation ("power3" by default)
+            },
             pin: true,
           },
         });
@@ -361,6 +366,7 @@ const ContentContainer = styled.div`
 
     .mockups-container {
       display: flex;
+      margin-top: 5rem;
       max-width: 100vw;
       overflow-x: hidden;
       ::-webkit-scrollbar {
